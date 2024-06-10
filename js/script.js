48373cc2b07c6747029ec14a16c4def20a28ba23
Monica Edem Kokovena <monicaekokovena@gmail.com>
2024-06-10 09:44:15 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const headerbtn = document.getElementById('headerbutton1');
+headerbtn.onclick = function () 
+{  alert('Thank You for downloading our E-Book!');
+}
+
+const headerbtn1 = document.getElementById('button2');
+headerbtn1.onclick = function () 
+{  alert('By clicking OK, this link is redirecting you to apple store');
+}
+
+const headerbtn2 = document.getElementById('button3');
+headerbtn2.onclick = function () 
+{  alert('By clicking OK, this link is redirecting you to play store');
+}
+
+
+const headerbtn3 = document.getElementById('butn11');
+headerbtn3.onclick = function () 
+{  alert('By clicking OK, you are agreeing to subscribe');
+}
+
+const headerbtn4 = document.getElementById('butn22');
+headerbtn4.onclick = function () 
+{  alert('By clicking OK, you are agreeing to download');
+}
+
+const headerbtn5 = document.getElementById('butn33');
+headerbtn5.onclick = function () 
+{  alert('By clicking OK, you are agreeing to download');
+}
+
+
+
+
+// const firstSectionScroll = document.getElementById('firstsection');
+// firstSectionScroll.onscroll = function () 
+// {  alert('Subscribe to our PREMIUM PACKAGE with a 10% discount just for today!');
+// }
+
+
+// document.getElementById("myDIV").onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = "You scrolled in div.";}
